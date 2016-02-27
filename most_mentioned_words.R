10fbb4d536e10e27d70c69ac6cc0afb5b4d07cc2
Lia Amelia <afifahlia@gmail.com>
2016-02-27 10:04:10 -0600

    R code update for most  mentioned words in tweet

--- most_mentioned_words.R
+++ most_mentioned_words.R
@@ -3,10 +3,10 @@ library('tm')
 twit.corpus <- Corpus(VectorSource(stem_result$stemming))
 twit.dtm <- TermDocumentMatrix(twit.corpus)
 twit.dtm
-#most used words
-muw <- findFreqTerms(twit.dtm,lowfreq=20000)
 
+#most mentioned words: words that are mention more than or equal to 7000 times
 z <- inspect( twit.dtm[ findFreqTerms(twit.dtm,lowfreq=7000), dimnames(twit.dtm)$Docs] )
 li <- rowSums(z)
 li <- sort(li, decreasing  = TRUE)
+#top 50 words mentioned >= 7000 times
 li[1:50]
